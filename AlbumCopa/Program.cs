c128ebc40799c3d303cd514912e8909a9a20fe71
Ana Carla Gomes da Silva <aninhacarla-ana@outlook.com>
2021-01-24 14:18:17 -0300

    esquema de pastas criado e Readme

---
+++ AlbumCopa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class Program {
+  
+    static void Main(string[] args) {
+        int numeroDeFigurinhas = Int32.Parse(Console.ReadLine());
+        int numeroDeFigurinhasCompradas = Int32.Parse(Console.ReadLine());
+        int totalDeFigurinhas = 0;
+
+        int[] albumDeFigurinha = new int[numeroDeFigurinhas];
+
+        
+            // IMPLEMENTE AQUI A SOLUÇÃO
+        for (int i = 0; i < numeroDeFigurinhasCompradas; i++) {
+            int figurinha = int.Parse(Console.ReadLine());
+            if (!albumDeFigurinha.Contains(figurinha)) {
+                albumDeFigurinha[i] = figurinha;
+                numeroDeFigurinhas -= 1;
+            }
+            if (numeroDeFigurinhas == 0) {
+                break;
+            }
+            }
+
+        totalDeFigurinhas = numeroDeFigurinhas;
+
+        Console.WriteLine(totalDeFigurinhas);
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+#Desafios de código em C# BootCamp GFT - Digital Innovation one
+
+<h1 align="center">Desafios e Soluções :books:</h1>
+
+
+<h3 align="left">Desenvolvendo Algoritmos com C#</h3>
+
+| Etapa |                              Desafio                              |     Solução     |
+|:---:|:------------------------:|:-------:|
+|  1  | Cálculo de Viagem | [Código]() |
+|  2  | Álbum da Copa | [Código]() |
+|  3  | Animal | [Código]() |
+
+
+
+### Desenvolvedoras: 
+
+| Ana Carla I [Github]() | [LinkedIn]()
+| Pamela |[Github]() | [LinkedIn]()
+| Tayane |[Github]() | [LinkedIn]()
+
+
